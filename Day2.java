f1ee6fd8498c9f28ae7ffa5bf083b2cbc5b6eeb5
Ciel-noob <indrasaputrasaputra98@gmail.com>
2026-09-03 13:21:44 +0800

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,19 @@
+public class Day2 {
+    public static void main(String[] args) {
+        // println dengan keterangan Biodata Diri
+        System.out.println("Biodata Diri");
+        System.out.println("Nama       : Indra Saputra");
+        System.out.println("Jurusan    : Teknik Informatika");
+        System.out.println("NIM        : D0226028");
+        System.out.println("Alumni     : SMAN 1 Majene");
+        System.out.println("Alamat     : Campalagian, Majene, Sulawesi Barat");
+
+        // print dengan keterangan Hobi
+        System.out.print("Hobi          : Baca Manhwa\n");
+        System.out.print("Manhwa Terbaik: The Greatest Estate Developer\n");
+        System.out.print("Rating        : 49/51\n");
+
+        // printf dengan keterangan lanjutan
+        System.out.printf("umur saya %d tahun%nWn Fav: %s%nManhua Fav: %s%n", 18, "The Fallen Lady Marigold", "My Wife is From a Thousand Years Ago");
+    }
+}
