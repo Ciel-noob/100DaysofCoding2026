bebf3edf7de1a44a95c4b09d28a34fc42e7db549
Ciel-noob <indrasaputrasaputra98@gmail.com>
2026-09-04 20:55:01 +0800

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,13 @@
+public class Day3 {
+public static void main(String[] args) {
+    System.out.println("C:\\Star\\Stream\n");
+    System.out.println("The Ugliest KIng\nKim Dokja\n");
+    System.out.println("Her is a Reader\tA Reader Remains a Reader \n");
+    System.out.println("Han Sooyoung: \"This Story is For One Reader\n");
+    System.out.println("Tell me.\\bAre you fine?\n");
+    System.out.println("Even if The FICTION became a REALITY\\rYou should never believe it's REAL\n");
+    System.out.println("I'll write the Epilogue for you\fUntil the end of Time\n");
+    System.out.println("Gojo\\Satoru\n");
+
+    }
+}
