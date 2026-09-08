a3664e400d8f3572d1e1396e520a0b22aad5d6d7
Ciel-noob <indrasaputrasaputra98@gmail.com>
2026-09-08 15:48:24 +0800

    Add files via upload

---
+++ Day7.java
@@ -0,0 +1,14 @@
+public class Day7 {
+    public static void main(String[] args) {
+        String k1 = "\"Reader\"";
+        String k2 ="This Story is For That One";
+        String k3 = "A Reader remains A ";
+
+        System.out.println("The Best Line in ORV: " + k2 + " " + k1);
+
+        String gabung = k2 + " " + k1;
+        System.out.println(gabung);
+        String gabung2 = k3 + k1;
+        System.out.println(gabung2);
+    }
+}
