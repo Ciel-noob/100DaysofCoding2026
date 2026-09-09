9b186e2246bbda85ef1ee7e5b165199fbd0039a8
Ciel-noob <indrasaputrasaputra98@gmail.com>
2026-09-09 11:28:19 +0800

    Add files via upload

---
+++ Day8.java
@@ -0,0 +1,22 @@
+public class Day8 {
+    public static void main(String[] args) {
+        // Deklarasi & Inisialisasi
+        String namaKarakter = "Kim dokja(Gwehh)";
+        int jumlahMyBiniGwehh = 7;
+
+        System.out.println("Nama Karakter: " + namaKarakter);
+        System.out.println("My Bini Gwehh = " + jumlahMyBiniGwehh);
+
+        //Update Variabel
+        jumlahMyBiniGwehh = 67;
+        System.out.println("Setelah Skenario Pertama: " + jumlahMyBiniGwehh);
+
+        namaKarakter = "Kim Gongja(GwehhBangetInimahhh)";
+        System.out.println("Setelah Skenario pertama: " + namaKarakter);
+        String k1 = "Ravielllll, Best Female Manhwa Character Everrr";
+        String k2 = "Need MyLady Raviel so HARDDDD";
+        System.out.println(k1);
+        System.out.println(k2);
+
+    }
+}
