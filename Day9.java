0717a1902cb776e6fd69f32750484aacfb371c9b
Ciel-noob <indrasaputrasaputra98@gmail.com>
2026-09-10 17:36:12 +0800

    Add files via upload

---
+++ Day9.java
@@ -0,0 +1,16 @@
+public class Day9 {
+    public static void main(String[] args) {
+        // Konstanta memakai final sehingga nilainya tidak boleh diubah lagi.
+        final String namaKarakter = "Kim Dokja";
+        final int pecahanGwehh = 49;
+        final int jumlahHari = 7;
+        final double jumlahBulan = 12;
+        final double nilaiPI = 3.14;
+
+        System.out.println("Nama karakter: " + namaKarakter);
+        System.out.println("Pecahan Gwehh= " + pecahanGwehh + "%");
+        System.out.println("Hari belajar= " + jumlahHari);
+        System.out.println("Jumlah bulan= " + jumlahBulan);
+        System.out.println("Nilai PI= " + nilaiPI);
+    }
+}
