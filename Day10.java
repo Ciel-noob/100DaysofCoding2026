27cc92c2418de89e8bc685b4436cd642e2145a34
Ciel-noob <indrasaputrasaputra98@gmail.com>
2026-09-11 16:23:06 +0800

    Add files via upload

---
+++ Day10.java
@@ -0,0 +1,16 @@
+public class Day10 {
+    public static void main(String[] args) {
+        // Nilai minimum dan maksimum dari beberapa tipe data
+                System.out.println("Nilai byte: " + Byte.MIN_VALUE + " sampai " + Byte.MAX_VALUE);
+                System.out.println("Nilai short: " + Short.MIN_VALUE + " sampai " + Short.MAX_VALUE);
+                System.out.println("Nilai int: " + Integer.MIN_VALUE + " sampai " + Integer.MAX_VALUE);
+                System.out.println("Nilai long: " + Long.MIN_VALUE + " sampai " + Long.MAX_VALUE);
+                System.out.println("Nilai float: " + Float.MIN_VALUE + " sampai " + Float.MAX_VALUE);
+                System.out.println("Nilai double: " + Double.MIN_VALUE + " sampai " + Double.MAX_VALUE);
+                System.out.println("char: " + (int) Character.MIN_VALUE + " sampai " + (int) Character.MAX_VALUE);
+                char nilai = 5149;
+                System.out.println((int) nilai / 100 + "/" + (int) nilai % 100);
+                String k1 = "Fragment Gwehh tuhh";
+                System.out.println(k1);
+        }    
+    }
