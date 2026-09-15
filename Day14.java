bad47c8d1cf348ffe8bdee6109c8a60a7ba0918d
Ciel-noob <indrasaputrasaputra98@gmail.com>
2026-09-15 21:21:55 +0800

    Update prompts and output messages in Day14.java

--- Day14.java
+++ Day14.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Day14 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Masukkan Angka 1: ");
+        System.out.println("Masukkan Angka Pertama: ");
         int a = input.nextInt();
-        System.out.println("Masukkan Angka 2: ");
+        System.out.println("Masukkan Angka Kedua: ");
         int b = input.nextInt();
 
         int c = a + b;
-        System.out.printf("%-15s: %s\n", "Angka 1 + Angka 2 = ", c);
+        System.out.printf("%-15s: %s\n", "Angka Pertama + Angka Kedua = ", c);
         int d = a - b;
-        System.out.printf("%-15s: %s\n", "Angka 1 - Angka 2 = ", d);
+        System.out.printf("%-15s: %s\n", "Angka Pertama - Angka Kedua = ", d);
     }
 }
